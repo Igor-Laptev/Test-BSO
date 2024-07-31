2c93396e73340651ddaf39626bc797dd88800a2b
IgorLB <igor101188@gmail.com>
2024-07-31 21:07:50 +0300

    productApi

--- my-app/src/Front/Api/productApi.ts
+++ my-app/src/Front/Api/productApi.ts
@@ -1,1 +1,16 @@
-//here productApi
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const productApi = createApi({
+  reducerPath: 'productApi',
+  baseQuery: fetchBaseQuery({ baseUrl: '/api' }),
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      query: () => 'products',
+    }),
+    getProductById: builder.query({
+      query: (id) => `products/${id}`,
+    }),
+  }),
+});
+
+export const { useGetProductQuery, useGetProductByIdQuery } = productApi;
