8e5f291d88f3d475547d44134bc09b920d8744fc
IgorLB <igor101188@gmail.com>
2024-07-31 21:49:33 +0300

    add register and login

--- my-app/src/Front/Route/Login.tsx
+++ my-app/src/Front/Route/Login.tsx
@@ -1,1 +1,34 @@
-//here login
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { login } from '../Store/authSlice';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+
+  const handleLogin = () => {
+    dispatch(login({ username, password }));
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <input
+        type='text'
+        placeholder='Username'
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type='password'
+        placeholder='Password'
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+
+export default Login;
