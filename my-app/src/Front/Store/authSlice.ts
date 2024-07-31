8b86cccb8e4d88cdc555ff01882474f21305d573
IgorLB <igor101188@gmail.com>
2024-07-31 21:23:05 +0300

    add authSlice

--- my-app/src/Front/Store/authSlice.ts
+++ my-app/src/Front/Store/authSlice.ts
@@ -1,1 +1,35 @@
-//here authSlice
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const login = createAsyncThunk('auth/login', async (credentials) => {
+  const response = await axios.post('/auth/login', credentials);
+  return response.data;
+});
+
+export const register = createAsyncThunk(
+  'auth/register',
+  async (credentials) => {
+    const response = await axios.post('/auth/register', credentials);
+    return response.data;
+  }
+);
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: {
+    user: null,
+    status: 'idle',
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(login.fulfilled, (state, action) => {
+        state.user = action.payload;
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.user = action.payload;
+      });
+  },
+});
+export default authSlice.reducer;
